7f7dd367a17ba6bbd8750196df8141a20f132f12
liis.kilp <kilp.liis@gmail.com>
2021-02-20 15:11:07 +0100

    add createScreens functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
+
 import './App.scss';
 import ImageBlock from './components/ImageBlock';
 import PageSlider from './components/PageSlider';
 import createScreens from './create-screens';
+import { selectCorrect, selectIncorrect, reset } from './store/score';
+
 
 const App = () => {
+  const correct = useSelector(selectCorrect);
+  const incorrect = useSelector(selectIncorrect);
+  const dispatch = useDispatch();
+
   const [newScreenIndex, setNewScreenIndex] = useState(0);
 
   const goToNextScreen = () => {
-    if (newScreenIndex === screens.length - 1) {
+    if (newScreenIndex === 0 || newScreenIndex === screens.length - 1) {
+      dispatch(reset());
       setNewScreenIndex(1);
     } else {
       setNewScreenIndex(newScreenIndex + 1);
     }
   }
 
-  const screens = createScreens(goToNextScreen);
+  const screens = createScreens(goToNextScreen, { correct, incorrect});
   const newScreen = screens[newScreenIndex];
 
 
--- src/create-screens.js
+++ src/create-screens.js
@@ -3,7 +3,6 @@ import QuestionBlock from './components/QuestionBlock';
 import ResultBlock from './components/ResultBlock';
 import questions from './questions.js';
 
-
 import netherlands from './assets/pictures/netherlands-pexels-photo-952632.jpeg';
 
 
@@ -13,13 +12,16 @@ const createScreen = (page, image) => ({
     image,
   })
   
-  const createScreens = (goToNextScreen) => {
+  const createScreens = (goToNextScreen, { correct, incorrect }) => {
     const screens = [];
     screens.push(createScreen(<IntroBlock goToNextScreen={goToNextScreen} />, netherlands));
     questions.forEach(question => {
       screens.push(createScreen(<QuestionBlock goToNextScreen={goToNextScreen} question={question}  />, question.image));
     })
-    screens.push(createScreen(<ResultBlock goToNextScreen={goToNextScreen} />, netherlands));
+    screens.push(createScreen(<ResultBlock
+        correct={correct}
+        incorrect={incorrect}
+        goToNextScreen={goToNextScreen} />, netherlands));
     return screens;
   }
 
