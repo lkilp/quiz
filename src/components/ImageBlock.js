c7c002d00a9484b197d40aafefb1d83c5e2d01ac
liis.kilp <kilp.liis@gmail.com>
2021-02-20 15:13:27 +0100

    cleanup and new dependencies

--- src/components/ImageBlock.js
+++ src/components/ImageBlock.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './ImageBlock.scss';
 
-import netherlands from '../assets/pictures/netherlands-pexels-photo-952632.jpeg';
-import PageSlider from './PageSlider';
-
 const ImageBlock = (props) => {
     const style = {
         backgroundImage: `url(${props.newImage})`,
--- src/components/PageSlider.js
+++ src/components/PageSlider.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import IntroBlock from './IntroBlock';
-
 import './PageSlider.scss';
-import QuestionBlock from './QuestionBlock';
-import ResultBlock from './ResultBlock';
 
 const PageSlider = (props) => {
     const { newPage } = props;
